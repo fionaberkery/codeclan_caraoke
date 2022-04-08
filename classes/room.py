1ec1e0d01264c356031e1b5ebebd5f0035f3e42e
Fiona <fiona_berkery@hotmail.com>
2022-04-08 15:08:44 +0100

    methods set up and tests passed for entry fee amounts added to room class and queue list added to room class and guest added to queue when  room at capacity

--- classes/room.py
+++ classes/room.py
@@ -1,16 +1,18 @@
 class Room:
-    def __init__(self, room_number, max_capacity):
+    def __init__(self, room_number, max_capacity, entry_fee):
         self.room_number = room_number
         self.max_capacity = max_capacity
+        self.entry_fee = entry_fee
         self.who_is_in_the_room = []
         self.songs_to_sing= []
+        self.queue = []
 
     def guest_list_count(self):
         return len(self.who_is_in_the_room)
     
     def check_in_guest(self, guest):
         if len(self.who_is_in_the_room) >= self.max_capacity:
-            return "Sorry no more room"
+            self.queue.append(guest)
         else:
             self.who_is_in_the_room.append(guest)
         
@@ -22,3 +24,6 @@ class Room:
 
     def add_song(self, song):
         self.songs_to_sing.append(song)
+
+    def how_many_in_the_queue(self):
+        return len(self.queue)
